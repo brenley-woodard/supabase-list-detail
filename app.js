490ab5fa30fe3599193f2a78b45cbef62bf42b86
brenley-woodard <brenleyew@gmail.com>
2022-11-14 16:56:10 -0800

    added html and js file for detail page, created html for home page, added async function to get foods from directory

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 /* Imports */
 
+import { getAllFoods } from './fetch-utils.js';
+
 /* Get DOM Elements */
 
 /* State */
 
 /* Events */
-
+window.addEventListener('load', async () => {
+    await getAllFoods();
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
